0b39665ef94dae3c4043ca969dd6c36f5d22eb13
Jake Gordon <jake@ozeki.ai>
2023-08-21 10:08:10 -0700

    added vue-router support

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <ul class="flex gap-4">
+    <li><router-link to="/">Home</router-link></li>
+    <li><router-link to="/about">About</router-link></li>
+  </ul>
+  <router-view></router-view>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import Home  from './page/Home.vue'
+import About from './page/About.vue'
+import App   from './App.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: "/",      component: Home  },
+  { path: "/about", component: About },
+]
+
+const router = createRouter({
+  routes: routes,
+  history: createWebHistory(),
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
