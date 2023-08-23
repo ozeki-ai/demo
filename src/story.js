34d4c95ef3c04e68faad96c53f47fd5a25ad0bec
Jake Gordon <jake@ozeki.ai>
2023-08-23 15:37:44 -0700

    make a more structured reactive object for story

--- src/story.js
+++ src/story.js
@@ -1,47 +1,70 @@
-import {ref} from "vue"
+import {reactive} from "vue"
 
 function run(storyFn) {
-  const story = ref(storyFn())
-  story.value.messages  = []
-  story.value.highlight = null
-  story.value.answering = false
-  step(story)
+  const story = reactive(new Story(storyFn()))
+  story.step()
   return story
 }
 
-function step(story) {
-  setTimeout(() => {
-    const command = story.value.script.shift()
-    if (command) {
-      switch (command.type) {
-        case "chat":
-          chatter(story, command)
-          break;
+//-------------------------------------------------------------------------------------------------
 
-        case "highlight":
-          story.value.highlight = command.id
-          step(story)
-          break;
+class Story {
 
-        case "answer":
-          story.value.answering = true
-          break;
+  constructor(config) {
+    this.title     = config.title
+    this.script    = config.script
+    this.sections  = config.sections
+    this.messages  = []
+    this.highlight = null
+    this.answering = false
+    this.index     = -1
+  }
+
+  step() {
+    setTimeout(() => {
+      this.index = this.index + 1
+      this.answering = false
+      if (this.index < this.script.length) {
+        const command = this.script[this.index]
+        switch (command.type) {
+          case "chat":
+            this.performChat(command)
+            break;
+          case "highlight":
+            this.performHighlight(command)
+            break;
+          case "answer":
+            this.collectAnswer(command)
+            break;
+        }
       }
-    }
-  }, 500)
+    }, 250)
+  }
+
+  performChat(command) {
+    const words = command.content.split(" ")
+    const index = this.messages.push({content: words.shift()}) - 1
+    const timer = setInterval(() => {
+      const word = words.shift()
+      this.messages[index].content += " " + word
+      if (words.length === 0) {
+        clearInterval(timer)
+        this.step()
+      }
+    }, 30)
+  }
+
+  performHighlight(command) {
+    this.highlight = command.id
+    this.step()
+  }
+
+  collectAnswer(command) {
+    this.answering = true
+  }
+
 }
 
-function chatter(story, command) {
-  const words = command.content.split(" ")
-  const index = story.value.messages.push({content: words.shift()}) - 1
-  const timer = setInterval(() => {
-    const word = words.shift()
-    story.value.messages[index].content += " " + word
-    if (words.length === 0) {
-      clearInterval(timer)
-      step(story)
-    }
-  }, 30)
-}
+//-------------------------------------------------------------------------------------------------
 
 export { run }
